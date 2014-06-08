56eb063cf3a5aa564d7d55e032dd59bf73f0364a
Lee Quarella <lee@lucidfrog.com>
2014-06-08 10:58:28 -0400

    improved attendance report

--- app/reports/attendence_percentage_report.rb
+++ app/reports/attendence_percentage_report.rb
@@ -1,15 +1,17 @@
 class AttendencePercentageReport
-  attr_accessor :practices, :skaters
+  attr_accessor :practices, :skaters, :start_date, :end_date
   def initialize(data)
-    @skaters = Skater.all
-    @practices = Practice.where("date > ? and date < ?", data[:start_date], data[:end_date])
+    @skaters    = Skater.all
+    @practices  = Practice.where("date > ? and date < ?", data[:start_date], data[:end_date])
+    @start_date = data[:start_date]
+    @end_date   = data[:end_date]
     calc
   end
 
   private
     def calc
       @skaters.each do |skater|
-        credit    = 0
+        credit = 0
         @practices.each do |practice|
           credit += 1 if skater.attended?(practice)
         end
