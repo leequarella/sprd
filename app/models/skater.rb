fc28561291c13096811fe638e7ae601c5aeefb40
Lee Quarella <lee@lucidfrog.com>
2014-06-08 15:31:41 -0400

    added roster eligibility calculations

--- app/models/skater.rb
+++ app/models/skater.rb
@@ -28,4 +28,28 @@ class Skater < ActiveRecord::Base
     end
     self.attendence_percentage = (credit.to_f/practices.length.to_f) * 100
   end
+
+  def roster_eligible? date_range
+    if attendence_percentage_for_day_of_week('Wed', date_range) == 100 &&
+       attendence_percentage_for_day_of_week('Sun', date_range) > 50
+      true
+    else
+      false
+    end
+  end
+
+  private
+  def attendence_percentage_for_day_of_week(day_of_week, date_range)
+    practices = Practice.where("date > ? and date < ?", date_range[:from], date_range[:to])
+    practices_count = 0
+    attended_count = 0
+    practices.each do |practice|
+      if practice.date.strftime('%a') == day_of_week
+        practices_count += 1
+        attended_count  += 1 if self.attended?(practice)
+      end
+    end
+    return 100 if practices_count == 0
+    (attended_count/practices_count)*100
+  end
 end
--- spec/models/skater_spec.rb
+++ spec/models/skater_spec.rb
@@ -43,4 +43,42 @@ RSpec.describe Skater, :type => :model do
       end
     end
   end
+
+  describe "roster_eligible?" do
+    let!(:skater) { Skater.create }
+    let!(:wed_practice_1) {Practice.create(date: DateTime.new(2014, 6, 4))}
+    let!(:wed_practice_2) {Practice.create(date: DateTime.new(2014, 6, 12))}
+    let!(:sun_practice_1) {Practice.create(date: DateTime.new(2014, 6, 7))}
+    let!(:sun_practice_2) {Practice.create(date: DateTime.new(2014, 6, 15))}
+    context "skater has made all wednesday practices and 50% of sunday practices" do
+      let!(:skater_practice_1) {SkaterPractice.create(skater: skater, practice: wed_practice_1,
+                                                   status: "attended")}
+      let!(:skater_practice_2) {SkaterPractice.create(skater: skater, practice: wed_practice_2,
+                                                   status: "attended")}
+      let!(:skater_practice_3) {SkaterPractice.create(skater: skater, practice:sun_practice_2,
+                                                   status: "attended")}
+      it "returns true" do
+        expect(skater.roster_eligible?(from: Date.new(2014, 6, 1), to: Date.new(2014, 7, 1)))
+          .to be true
+      end
+    end
+    context "skater has missed a wednesday practice" do
+      let!(:skater_practice) {SkaterPractice.create(skater: skater, practice:wed_practice_1,
+                                                   status: "absent")}
+      it "returns false" do
+        expect(skater.roster_eligible?(from: Date.new(2014, 6, 1), to: Date.new(2014, 7, 1)))
+          .to be false
+      end
+    end
+    context "skater has missed a more than 50% of sundays" do
+      let!(:skater_practice_1) {SkaterPractice.create(skater: skater, practice: wed_practice_1,
+                                                   status: "attended")}
+      let!(:skater_practice_2) {SkaterPractice.create(skater: skater, practice: wed_practice_2,
+                                                   status: "attended")}
+      it "returns false" do
+        expect(skater.roster_eligible?(from: Date.new(2014, 6, 1), to: Date.new(2014, 7, 1)))
+          .to be false
+      end
+    end
+  end
 end
