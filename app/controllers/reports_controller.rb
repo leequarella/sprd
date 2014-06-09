1a3b7775465580568f9f5b4d250dea77481a532e
Lee Quarella <lee@lucidfrog.com>
2014-06-08 22:46:21 -0400

    today

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,7 +5,7 @@ class ReportsController < ApplicationController
       end_date   = date_from_params params[:end_date]
     else
       start_date = 1.month.ago
-      end_date   = 1.day.from_now
+      end_date   = Date.today
     end
     @report = AttendencePercentageReport.new(start_date: start_date,
                                              end_date:   end_date)
